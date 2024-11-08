0a37b7d45ae501230aa29302d8b09c22fc3a15d2
Emanuel Ramos Paiva <emanuelramospaiva@gmail.com>
2024-11-08 14:57:55 -0300

    Dto de pedido

---
+++ book/src/main/java/br/grupointegrado/book/DTO/PedidoRequestDTO.java
@@ -0,0 +1,4 @@
+package br.grupointegrado.book.DTO;
+
+public record PedidoRequestDTO(String status) {
+}
--- book/src/main/java/br/grupointegrado/book/controller/PedidosController.java
+++ book/src/main/java/br/grupointegrado/book/controller/PedidosController.java
@@ -1,17 +1,16 @@
 package br.grupointegrado.book.controller;
 
 
+import br.grupointegrado.book.DTO.PedidoRequestDTO;
 import br.grupointegrado.book.model.Pedidos;
 import br.grupointegrado.book.repository.PedidosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Pedidos")
+@RequestMapping("/api/pedidos")
 public class PedidosController {
 
     @Autowired
@@ -20,6 +19,21 @@ public class PedidosController {
     @GetMapping
     public List<Pedidos> findAll() {
         return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Pedidos findById(@PathVariable Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Filme não foi encontrado"));
+    }
+
+    @PostMapping
+    public Pedidos save(@RequestBody PedidoRequestDTO dto) {
+        Pedidos pedidos = new Pedidos();
+        pedidos.setStatus(dto.status());
+
+        return this.repository.save(dto);
     }
 
 }
